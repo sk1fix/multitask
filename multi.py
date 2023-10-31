7712bc7816fde86b298dd6eecd47398545b10811
sk1fix <agafonovtoha427@gmail.com>
2023-10-31 12:50:18 +0400

    mat

--- multi.py
+++ multi.py
@@ -4,3 +4,16 @@ def sum_str(x,a):
         sum+=a[x][j]
     return sum
 
+if "__main__"==__name__:
+    rows=int(input())
+    columns=int(input())
+    mat=[]
+    for i in range(columns):
+        elem=[]
+        for j in range(rows):
+            a=int(input())
+            elem.append(a)
+        mat.append(elem)
+    for i in range(columns):
+        print(*mat[i])
+    
